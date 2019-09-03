1c7ef306fb12dafcfdc206648fe8ee72f89a93a9
Rais <raisdeshmukh@gmail.com>
2019-09-03 23:29:57 +0530

    Add setter and Getter and to String

--- BusProvider/src/main/java/com/bp/bo/BusProviderBo.java
+++ BusProvider/src/main/java/com/bp/bo/BusProviderBo.java
@@ -11,6 +11,41 @@ protected String bussinessName;
 protected String primaryContactNo;
 protected String primaryEmailAddress;
 protected String status;
+public int getBusProviderNo() {
+	return busProviderNo;
+}
+public void setBusProviderNo(int busProviderNo) {
+	this.busProviderNo = busProviderNo;
+}
+public String getBussinessName() {
+	return bussinessName;
+}
+public void setBussinessName(String bussinessName) {
+	this.bussinessName = bussinessName;
+}
+public String getPrimaryContactNo() {
+	return primaryContactNo;
+}
+public void setPrimaryContactNo(String primaryContactNo) {
+	this.primaryContactNo = primaryContactNo;
+}
+public String getPrimaryEmailAddress() {
+	return primaryEmailAddress;
+}
+public void setPrimaryEmailAddress(String primaryEmailAddress) {
+	this.primaryEmailAddress = primaryEmailAddress;
+}
+public String getStatus() {
+	return status;
+}
+public void setStatus(String status) {
+	this.status = status;
+}
+@Override
+public String toString() {
+	return "BusProviderBo [busProviderNo=" + busProviderNo + ", bussinessName=" + bussinessName + ", primaryContactNo="
+			+ primaryContactNo + ", primaryEmailAddress=" + primaryEmailAddress + ", status=" + status + "]";
+}
 
 
 
